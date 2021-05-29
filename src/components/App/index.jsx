c5f1630abaec22c65df9052d19ee9062c42866db
Vilena Taraskina <vilena_taraskina@mail.ru>
2021-05-29 10:41:54 +0200

    Router

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,11 +2,26 @@ import React from 'react';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
 import { Home } from '../Home';
+import { Reservation } from '../Reservation';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
 export const App = () => (
-  <>
+  <Router>
     <Header />
-    <Home />
+    <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/reservation">
+          <Reservation />
+        </Route>
+      </Switch>
     <Footer />
-  </>
+  </Router>
 );
