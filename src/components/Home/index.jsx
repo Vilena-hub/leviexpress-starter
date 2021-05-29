29cec4a6ab8fcb275c7fd71bfb36fabef8cf167d
Vilena Taraskina <vilena_taraskina@mail.ru>
2021-05-29 14:15:30 +0200

    Výběr data cesty

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { JourneyPicker } from '../JourneyPicker';
 
-export const Home = () => (
+export const Home = () => {
+
+  const [journey, setJourney] = useState(null);
+
+  return (
   <main>
     <JourneyPicker />
+    
+    {
+      journey===null
+      ? " "
+      : <div>Ahoj!</div>
+    }
+
   </main>
-);
+  )
+};
