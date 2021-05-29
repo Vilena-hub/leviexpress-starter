79cc5b6cd1e21f08dcd9beabe5c191e72e737782
Vilena Taraskina <vilena_taraskina@mail.ru>
2021-05-29 12:36:20 +0200

    Výběr města

--- src/components/JourneyPicker/index.jsx
+++ src/components/JourneyPicker/index.jsx
@@ -2,36 +2,71 @@ import React, { useEffect, useState } from 'react';
 import mapImage from './img/map.svg';
 import './style.css';
 
-export const JourneyPicker = ({ onJourneyChange }) => (
+const CityOptions = ({cities}) => {
+  return (
+    <>
+            <option value="">Vyberte</option>
+            {cities.map(mesto=><option key={mesto.code} value={mesto.code}>{mesto.name}</option>)}
+    </>
+  )
+ };
+
+
+export const JourneyPicker = ({ onJourneyChange }) => {
+
+  useEffect (
+   () => {
+     fetch('https://leviexpress-backend.herokuapp.com/api/cities')
+     .then(response=>response.json())
+     .then(json=>setCities(json.data))
+   },
+   []
+  )
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('formulář odeslán')
+    console.log(fromCity,toCity,date);
+  }
+
+  const [fromCity, setFromCity]=useState(" ");
+  const [toCity, setToCity]=useState(" ");
+  const [date, setDate]=useState(" ");
+  const [cities, setCities]=useState([]);
+
+  const handleFromCity = (event) => {
+    setFromCity(event.target.value);
+  }
+
+  const handleToCity = (event) => {
+    setToCity(event.target.value);
+  }
+
+  const handleDate = (event) => {
+    setDate(event.target.value);
+  }
+   return (
   <div className="journey-picker container">
     <h2 className="journey-picker__head">Kam chcete jet?</h2>
     <div className="journey-picker__body">
-      <form className="journey-picker__form">
+      <form onSubmit={handleSubmit} className="journey-picker__form">
         <label>
           <div className="journey-picker__label">Odkud:</div>
-          <select>
-            <option value="">Vyberte</option>
-            <option value="mesto01">Město 01</option>
-            <option value="mesto02">Město 02</option>
-            <option value="mesto03">Město 03</option>
-            <option value="mesto04">Město 04</option>
-            <option value="mesto05">Město 05</option>
+          <select value={fromCity} onChange={handleFromCity}>
+            <CityOptions cities={cities}/>
+
           </select>
         </label>
         <label>
           <div className="journey-picker__label">Kam:</div>
-          <select>
-            <option value="">Vyberte</option>
-            <option value="mesto01">Město 01</option>
-            <option value="mesto02">Město 02</option>
-            <option value="mesto03">Město 03</option>
-            <option value="mesto04">Město 04</option>
-            <option value="mesto05">Město 05</option>
+          <select value={toCity} onChange={handleToCity}>
+            <CityOptions cities={cities}/>
+
           </select>
         </label>
         <label>
           <div className="journey-picker__label">Datum:</div>
-          <select>
+          <select value={date} onChange={handleDate}>
             <option value="">Vyberte</option>
             <option value="datum01">Datum 01</option>
             <option value="datum02">Datum 02</option>
@@ -52,4 +87,4 @@ export const JourneyPicker = ({ onJourneyChange }) => (
       <img className="journey-picker__map" src={mapImage} />
     </div>
   </div>
-);
+   )};
